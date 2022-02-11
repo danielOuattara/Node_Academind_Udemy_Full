03216c5d6cb248b316d97e4eeed38299333dc675
daniel ouattara <daniel.ouattara@gmx.com>
2022-02-11 23:03:38 +0100

    Section 14: cookie Part= DONE, next is session

---
+++ 14_Session_and_Cookies/course/controllers/auth.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+
+
+exports.getLogin = (req, res, next) => {
+    let isLoggedIn = false;
+    if (req.get('Cookie')) {
+        let cookieArray = req.get('Cookie').split(';');
+        cookieArray.forEach((cookie) => {
+            if (cookie.includes('isLoggedIn')) {
+                isLoggedIn = cookie.split('=')[1] === 'true';
+                return isLoggedIn;
+            }
+        })
+    }
+    res.render('auth/login', {
+        pageTitle: 'login',
+        path: '/login',
+        isAuthenticated: isLoggedIn
+    });
+}
+
+
+exports.postLogin = (req, res, next) => {
+    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'isLoggedIn=true')
+    res.redirect('/');
+};
