e12c89dcb74dae610d4f8779d9cea4f3decd27bf
daniel ouattara <daniel.ouattara@gmx.com>
2022-02-14 04:59:14 +0100

    corrections+update. Pug is OK, next others: EJS...

---
+++ 06_dyamic_content_Adding_Template_Engine/case_01_starting/app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(5500, () => {
+    console.log('App is running on http://localhost:5500/')
+});
