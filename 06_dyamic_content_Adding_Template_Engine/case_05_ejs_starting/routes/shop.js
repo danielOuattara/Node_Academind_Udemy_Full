501bf2eb4d8e41592aa7eaa10ab505127260ca65
daniel ouattara <daniel.ouattara@gmx.com>
2022-02-14 08:40:51 +0100

    EJS OK. Next = Section 7 MVC for updates & correct

---
+++ 06_dyamic_content_Adding_Template_Engine/case_05_ejs_starting/routes/shop.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+const router = express.Router();
+const adminData = require('./admin');
+
+
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products)
+  res.render('shop', {
+    products: adminData.products,
+    pageTitle: 'Shop',
+  });
+});
+
+module.exports = router;
