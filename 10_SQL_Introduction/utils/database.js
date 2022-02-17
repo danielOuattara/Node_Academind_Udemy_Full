c17ab9cb9026f6c38e934167c56a5c15d9d0dbd6
Daniel OUATTARA <67764936+danielOuattara@users.noreply.github.com>
2022-02-17 15:44:43 +0100

    Delete database.js
    delete sensitive info

--- 10_SQL_Introduction/utils/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: `academind_node_express_udemy_full`,
-    password: "1_superPassword!"
-});
-
-module.exports = pool.promise();
