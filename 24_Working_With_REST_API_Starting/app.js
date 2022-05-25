117427571a781ad7ae6f505c8b97740efff70553
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-25 18:21:19 +0200

    REST_API starting: completed

---
+++ 24_Working_With_REST_API_Starting/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const feedRoutes = require("./routes/feed");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT,PATCHC, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  next()
+});
+
+app.use("/api/v1/", feedRoutes);
+
+app.listen(8080, () =>
+  console.log(`App is running on port http://localhost:${8080}/`)
+);
