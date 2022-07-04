ce8af2161cade3b9926cb2aa14528091fd750ab0
danielOuattara <daniel.ouattara@gmx.com>
2022-07-04 23:36:19 +0200

    typo

--- 25_REST_API_Practical_Application/backend/app.js
+++ 25_REST_API_Practical_Application/backend/app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT,PATCHC, DELETE"
+    "GET, POST, PUT,PATCH, DELETE"
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
