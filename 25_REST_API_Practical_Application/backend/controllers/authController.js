aacb504b57ac2de9d41f6c97c4a3c53f06fb115e
danielOuattara <daniel.ouattara@gmx.com>
2022-07-05 10:17:23 +0200

    hashing password with bcryptjs

--- 25_REST_API_Practical_Application/backend/controllers/authController.js
+++ 25_REST_API_Practical_Application/backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 const User = require("./../models/userModel");
 const Post = require("./../models/postModel");
+const bcyptjs = require("bcryptjs");
 
 //-----------------------------------------------------------
 exports.signUp = (req, res, next) => {
@@ -8,11 +9,28 @@ exports.signUp = (req, res, next) => {
   if (!errors.isEmpty()) {
     const error = new Error(errors.array()[0].msg);
     error.statusCode = 422;
-    error.data = error.array();
+    error.data = errors.array();
     throw error;
   }
 
   const { email, name, password } = req.body;
+  bcyptjs
+    .hash(password, 11)
+    .then((hashedPassword) => {
+      console.log("Here");
+      return User.create({ email, name, password: hashedPassword });
+    })
+    .then((user) =>
+      res
+        .status(201)
+        .json({ user, message: "user successfully created! " })
+    )
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
 };
 
 //--------------------------------------------------------
