c66eee6251f04eedbb5e58bc1ead5178b0537d8f
danielOuattara <daniel.ouattara@gmx.com>
2022-07-05 12:22:59 +0200

    adding "isAuth"  authe middleware to all routes

--- 25_REST_API_Practical_Application/backend/routes/feedRoute.js
+++ 25_REST_API_Practical_Application/backend/routes/feedRoute.js
@@ -3,18 +3,26 @@ const router = express.Router();
 const feedController = require("../controllers/feedController");
 const { postValidation } = require("./../middlewares/validators");
 const multer = require("./../middlewares/multer-config");
+const isAuth = require("./../middlewares/isAuth.js");
 
 //--------------------------------------------------------------
-router.get("/posts", feedController.getPosts);
-router.post("/post", multer, postValidation, feedController.createPost);
-router.get("/post/:postId", feedController.getOnePost);
+router.get("/posts", isAuth, feedController.getPosts);
+router.post(
+  "/post",
+  isAuth,
+  multer,
+  postValidation,
+  feedController.createPost
+);
+router.get("/post/:postId", isAuth, feedController.getOnePost);
 router.put(
   "/post/:postId",
+  isAuth,
   multer,
   postValidation,
   feedController.updatePost
 );
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 //--------------------------------------------------------------
 module.exports = router;
