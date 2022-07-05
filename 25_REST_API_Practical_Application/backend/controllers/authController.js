3d9368341d3d21652b1b5b69b8c847f79b27cdd7
danielOuattara <daniel.ouattara@gmx.com>
2022-07-05 10:35:36 +0200

    typo correct + adding "login" controller

--- 25_REST_API_Practical_Application/backend/controllers/authController.js
+++ 25_REST_API_Practical_Application/backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const { validationResult } = require("express-validator");
 const User = require("./../models/userModel");
 const Post = require("./../models/postModel");
-const bcyptjs = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 
 //-----------------------------------------------------------
 exports.signUp = (req, res, next) => {
@@ -14,7 +14,7 @@ exports.signUp = (req, res, next) => {
   }
 
   const { email, name, password } = req.body;
-  bcyptjs
+  bcryptjs
     .hash(password, 11)
     .then((hashedPassword) => {
       console.log("Here");
@@ -31,6 +31,55 @@ exports.signUp = (req, res, next) => {
       }
       next(error);
     });
+};
+
+//-----------------------------------------------------------
+exports.login = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error(errors.array()[0].msg);
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User not Found");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      return bcryptjs.compare(password, user.password);
+    })
+    .then((match) => {
+      if (!match) {
+        const error = new Error("Unauthorized");
+        error.statusCode = 401;
+        throw error;
+      }
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+
+  bcyptjs
+    .hash(password, 11)
+    .then((hashedPassword) => {
+      console.log("Here");
+      return User.create({ email, name, password: hashedPassword });
+    })
+    .then((user) =>
+      res
+        .status(201)
+        .json({ user, message: "user successfully created! " })
+    );
 };
 
 //--------------------------------------------------------
