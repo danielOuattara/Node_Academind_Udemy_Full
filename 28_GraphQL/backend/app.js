f307bf609fbd719df91d87f04e7f3bf9dccc0aab
danielOuattara <daniel.ouattara@gmx.com>
2022-07-28 23:52:49 +0200

    cleaning routes &socket.io

--- 28_GraphQL/backend/app.js
+++ 28_GraphQL/backend/app.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const feedRoutes = require("./routes/feedRoute");
-const authRoutes = require("./routes/authRoute");
-const userRoutes = require("./routes/userRoute");
 const mongoose = require("mongoose");
 const path = require("path");
 
@@ -30,11 +27,6 @@ app.use((req, res, next) => {
 // serving images statiscally
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-//-----------------------------------
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/feed", feedRoutes);
-app.use("/api/v1/user", userRoutes);
-
 //------------------------------------
 app.use((error, req, res, next) => {
   console.log(error);
@@ -49,18 +41,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB Database: success !");
-    const server = app.listen(8080, () =>
+    app.listen(8080, () =>
       console.log(`App is running on port http://localhost:${8080}/`)
     );
-
-    //// npm install socket.io for backend + configure optionally here
-    //* Now app.js requestes 'io' from 'socket.io' and call init() function
-
-    const io = require("./socket").init(server);
-    // console.log("io = ", io);
-    io.on("connection", (socket) => {
-      console.log("Client connected");
-      // console.log(socket);
-    });
   })
   .catch((err) => console.log(err));
