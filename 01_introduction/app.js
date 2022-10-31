33fb0c49a6261879172b217dc0efe5b74e19b43a
danielOuattara <daniel.ouattara@gmx.com>
2022-10-31 20:52:56 +0100

    revisited

--- 01_introduction/app.js
+++ 01_introduction/app.js
@@ -1,12 +1,11 @@
 console.log("Hello");
 
-
-const { writeFile, writeFileSync } = require('fs');
+const { writeFile, writeFileSync } = require("fs");
 
 writeFile("async_test.txt", "Hello Node", (err, data) => {
-    if(err) {
-        console.log(err)
-    }
+  if (err) {
+    console.log(err);
+  }
 });
 
 writeFileSync("sync_test.txt", "Hello Node Again ");
