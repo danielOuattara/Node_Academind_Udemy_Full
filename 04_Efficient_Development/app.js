b8c5ecedf53bab05bb1a5caaa5f14b14645df21c
danielOuattara <daniel.ouattara@gmx.com>
2022-11-01 16:23:09 +0100

    revisited + about debugging.

--- 04_Efficient_Development/app.js
+++ 04_Efficient_Development/app.js
@@ -1,9 +1,8 @@
-
-const http = require('http');
-const requestHandler = require('./routes')
+const http = require("http");
+const requestHandler = require("./routes");
 const server = http.createServer(requestHandler);
 
 const PORT = 5500;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`);
 });
--- 04_Efficient_Development/routes.js
+++ 04_Efficient_Development/routes.js
@@ -1,44 +1,50 @@
-const { writeFile } = require ('fs');
+const { writeFile } = require("fs");
 
 function requestHandler(req, res) {
-    const url = req.url;
-    const method = req.method;
-    res.setHeader('Content-Type', 'text/html');
-    
-    if (url === "/") {
-        res.write("<html>");
-        res.write("<head><meta charset='UTF-8'><title>Enter Message</title></head>");
-        res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><input type='submit' value='send Message'/></form></body>")
-        res.write("</html>")
+  const url = req.url;
+  const method = req.method;
+  res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    res.write(`
+        <html>
+            <head>
+                <title>Enter Message</title>
+            </head>
+            <body>
+                <form action='/message' method='POST'>
+                    <input type='text' name='message'/>
+                    <input type='submit' value='send Message'/>
+                </form>
+            </body>
+        </html>`);
+    res.end();
+  } else if (url == "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk ==", chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1].replace(/\++/g, " ");
+      writeFile("message.txt", `${message}\n`, { flag: "a" }, (err, data) => {
+        if (err) console.log(err);
+        res.statusCode = 302;
+        res.writeHead(302, { Location: "/" });
         res.end();
-    
-    } else if (url == '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log("chunk ==", chunk)
-            body.push(chunk)
-        });
-        
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody)
-            const message = parsedBody.split("=")[1].replace(/\++/g, " ");
-            writeFile('message.txt', `${message}\n`, { flag: 'a'}, (err, data) => {
-                if (err) console.log(err)
-                res.statusCode = 302;
-                res.writeHead(302, { 'Location': '/' });
-                res.end();
-            });
-        });
-    
-    } else {
-        // res.setHeader('Content-Type', 'text/html');
-        res.write("<html>");
-        res.write("<head><title>My first page</title></head>");
-        res.write("<body><h1>Hello Node.js </h1></body>")
-        res.write("</html>")
-        res.end();
-    }
+      });
+    });
+  } else {
+    // res.setHeader('Content-Type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write("<body><h1>Hello Node.js </h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 }
 
 module.exports = requestHandler;
