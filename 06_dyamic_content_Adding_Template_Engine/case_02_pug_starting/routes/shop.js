d250e1b706eebb626243ea36e731930825003c21
danielOuattara <daniel.ouattara@gmx.com>
2022-11-03 10:50:28 +0100

    " pug " revisited + optimized

--- 06_dyamic_content_Adding_Template_Engine/case_02_pug_starting/routes/shop.js
+++ 06_dyamic_content_Adding_Template_Engine/case_02_pug_starting/routes/shop.js
@@ -1,18 +1,17 @@
-const path = require('path');
-const express = require('express');
-const rootDir = require('../util/path');
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
 const router = express.Router();
-const adminData = require('./admin');
-
-
+const adminData = require("./admin");
 
 // router.get('/', (req, res, next) => {
 //   console.log(adminData.products)
 //   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 // });
 
-router.get('/', (req, res, next) => {
-  res.render('shop', {products: adminData.products, docTitle: 'Shop'});
+// render a 'pug' viewS
+router.get("/", (req, res, next) => {
+  res.render("shop", { products: adminData.products, docTitle: "Shop" });
 });
 
 module.exports = router;
--- 06_dyamic_content_Adding_Template_Engine/case_03_pug_layout/routes/admin.js
+++ 06_dyamic_content_Adding_Template_Engine/case_03_pug_layout/routes/admin.js
@@ -1,25 +1,23 @@
-const path = require('path');
-const express = require('express');
-const rootDir = require('../util/path');
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
 const router = express.Router();
 
-
-
-const products =  [];
+const products = [];
 
 // /admin/add-product => GET
 // router.get('/add-product', (req, res, next) => {
 //   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 // });
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {pageTitle:"Add-product", path: 'add-product'});
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", { pageTitle: "Add-product", path: "add-product" });
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({title: req.body.title})
-  res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
 // module.exports = router;
