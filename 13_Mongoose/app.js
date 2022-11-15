bcbba867e2df1ebf613caa81f444f40428bd8f03
danielOuattara <daniel.ouattara@gmx.com>
2022-11-15 13:32:20 +0100

    adding populate tests users 3 differents ways

--- 13_Mongoose/app.js
+++ 13_Mongoose/app.js
@@ -1,24 +1,154 @@
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const User = require("./models/userModel");
+// const errorControllers = require("./controllers/errorControllers");
+
+// const app = express();
+
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// const adminRoutes = require("./routes/adminRoutes");
+// const shopRoutes = require("./routes/shopRoutes");
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static("public"));
+
+// app.use((req, res, next) => {
+//   User.findById("6373863da0a6a98f3fd2503c")
+//     .then((user) => {
+//       req.user = user; // here user is a full mongoose model: with all method and properties
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorControllers.get404);
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     return User.findOne();
+//   })
+//   .then((user) => {
+//     if (!user) {
+//       const user = new User({
+//         name: "Daniel",
+//         email: "daniel@email.com",
+//         cart: {
+//           items: [],
+//         },
+//       });
+//       return user.save();
+//     }
+//   })
+//   .then(() => {
+//     console.log("Connection to MongoDB database : SUCCESS ");
+//     app.listen(3000, () =>
+//       console.log("App is running on port http://localhost:3000/")
+//     );
+//   })
+//   .catch((err) => console.log(err));
+
+//==
+
+//---------------------------------------------------------- OK
+
+// require("dotenv").config();
+// const express = require("express");
+// const User = require("./models/userModel");
+// const mongoose = require("mongoose");
+// const adminRoutes = require("./routes/adminRoutes");
+// const shopRoutes = require("./routes/shopRoutes");
+// const errorControllers = require("./controllers/errorControllers");
+
+// const app = express();
+
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static("public"));
+
+// // simulating logging for a user
+// app.use((req, res, next) => {
+//   User.findById("6373863da0a6a98f3fd2503c") // user manually created in Compass
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       console.log("user = ", user);
+//       // Allows to access all method and properties available through assiociation
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorControllers.get404);
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     return User.find();
+//   })
+//   .then((users) => {
+//     // if (users.length === 0) {
+//     //   return Promise.all([
+//     //     new User({ name: "Daniel", email: "daniel@email.com" }).save(),
+//     //     new User({ name: "Julie", email: "julie@email.com" }).save(),
+//     //     new User({ name: "Gaïa", email: "gaia@email.com" }).save(),
+//     //     new User({ name: "Amaya", email: "amaya@email.com" }).save(),
+//     //   ]);
+//     // }
+//     if (users.length === 0) {
+//       return Promise.all([
+//         User.create({ name: "Daniel", email: "daniel@email.com" }),
+//         User.create({ name: "Julie", email: "julie@email.com" }),
+//         User.create({ name: "Gaïa", email: "gaia@email.com" }),
+//         User.create({ name: "Amaya", email: "amaya@email.com" }),
+//       ]);
+//     } else {
+//       return users;
+//     }
+//   })
+//   .then((users) => {
+//     app.listen(3000);
+//   })
+//   .then(() => console.log("App is running over http://localhost:3000/"))
+//   .catch((err) => console.log(err));
+
+//---------------------------------------------------------- OK
+
 require("dotenv").config();
-const path = require("path");
 const express = require("express");
-const errorController = require("./controllers/error");
-const mongoose = require("mongoose");
-const User = require("./models/user");
+const User = require("./models/userModel");
+const adminRoutes = require("./routes/adminRoutes");
+const shopRoutes = require("./routes/shopRoutes");
+const errorControllers = require("./controllers/errorControllers");
+const usersTestData = require("./data/usersTest.json");
+const { default: mongoose } = require("mongoose");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
-  User.findById("6208db9bad6fc9fa11177cbc")
+  User.findById("6373863da0a6a98f3fd2503c") // user manually created in Compass
     .then((user) => {
-      req.user = user; // here user is a full mongoose model: with all method and properties
+      // console.log("user = ", user);
+      // req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      console.log("user = ", user);
+      // Allows to access all method and properties available through assiociation
       next();
     })
     .catch((err) => console.log(err));
@@ -27,30 +157,34 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404);
+app.use(errorControllers.get404);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    return User.findOne();
+    return User.find();
   })
-  .then((user) => {
-    if (!user) {
-      const user = new User({
-        name: "Daniel",
-        email: "daniel@email.com",
-        cart: {
-          items: [],
-        },
+  .then((users) => {
+    // if (users.length === 0) {
+    //   // initializing users for development
+    //   return usersTestData.map((person) => {
+    //     const { name, email } = person;
+    //     return new User({ name, email }).save();
+    //   });
+    // }
+    if (users.length === 0) {
+      // initializing users for development
+      return usersTestData.map((person) => {
+        const { name, email } = person;
+        return User.create({ name, email });
       });
-      return user.save();
+    } else {
+      return users;
     }
   })
-  .then(() => {
-    console.log("Connection to MongoDB database : SUCCESS ");
-    app.listen(3000, () =>
-      console.log("App is running on port http://localhost:3000/")
-    );
+  .then((users) => {
+    console.log("users = ", users);
+    app.listen(3000);
   })
+  .then(() => console.log("App is running over http://localhost:3000/"))
   .catch((err) => console.log(err));
-// console.log("Connected to MongoDB Database: success !");s
