0294cacb330015f04406deb0393fb8352af48082
danielOuattara <daniel.ouattara@gmx.com>
2022-12-02 09:03:31 +0100

    model formatting

--- 20_File_Upload_and_Download/models/product.js
+++ 20_File_Upload_and_Download/models/product.js
@@ -1,29 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    title:  {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required: true
-    }
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
-module.exports =  mongoose.model('Product', productSchema);
-
-
+module.exports = mongoose.model("Product", productSchema);
