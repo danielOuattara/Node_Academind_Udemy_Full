52a00efd5cdf0d75471198b9ec8011d04c481016
danielOuattara <daniel.ouattara@gmx.com>
2022-12-02 19:31:44 +0100

    updated according to new admin controller design

--- 20_File_Upload_and_Download/routes/admin.js
+++ 20_File_Upload_and_Download/routes/admin.js
@@ -1,22 +1,14 @@
-const path = require("path");
 const express = require("express");
-const adminController = require("../controllers/admin");
+// const adminController = require("../controllers/admin"); // BACKUP
+const adminController = require("./../controllers/admin_controllers/");
 const router = express.Router();
 const { productValidate } = require("./../middlewares/validators");
 
-// /admin/add-product => GET
 router.get("/add-product", adminController.getAddProduct);
-
-// /admin/add-product => POST
 router.post("/add-product", productValidate, adminController.postAddProduct);
-
-// /admin/products => GET
 router.get("/products", adminController.getProducts);
-
 router.get("/edit-product/:productId", adminController.getEditProduct);
-
 router.post("/edit-product", productValidate, adminController.postEditProduct);
-
 router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
