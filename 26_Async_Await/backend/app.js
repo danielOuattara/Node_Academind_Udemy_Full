298888f1d2675cb0911df896a832a4d2a272e1de
danielOuattara <daniel.ouattara@gmx.com>
2022-12-06 14:51:27 +0100

    updates & corrections

--- 26_Async_Await/backend/app.js
+++ 26_Async_Await/backend/app.js
@@ -12,14 +12,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT,PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
@@ -50,7 +44,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB Database: success !");
     app.listen(8080, () =>
-      console.log(`App is running on port http://localhost:${8080}/`)
+      console.log(`App is running on port http://localhost:${8080}/`),
     );
   })
   .catch((err) => console.log(err));
--- 26_Async_Await/backend/controllers/userController.js
+++ 26_Async_Await/backend/controllers/userController.js
@@ -4,14 +4,13 @@ const User = require("./../models/userModel");
 //--------------------------------------------------------
 exports.getStatus = async (req, res, next) => {
   try {
-    const user = await User.findById(req.userId).then((user) => {
-      if (!user) {
-        const error = new Error("User Unknown");
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({ status: user.status });
-    });
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error("User Unknown");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ status: user.status });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 500;
@@ -26,10 +25,16 @@ exports.updateStatus = async (req, res, next) => {
   if (!errors.isEmpty()) {
     const error = new Error(errors.array()[0].msg);
     error.statusCode = 422;
-    throw error;
+    // throw error;
+    next(error);
   }
 
   try {
+    if (!req.body.status) {
+      const error = new Error("Bad Request: Status text is required");
+      error.statusCode = 400;
+      throw error;
+    }
     const user = await User.findById(req.userId);
     if (!user) {
       const error = new Error("User Unknown");
