93410274f8a5ff0f6ba8fcdf430a8bb3ef532454
danielOuattara <daniel.ouattara@gmx.com>
2024-01-15 16:12:31 +0100

    34 Deno Intro: update code for 01-first-deno-code

--- 34_Introduction_to_Deno/01-first-deno-code/app_02.js
+++ 34_Introduction_to_Deno/01-first-deno-code/app_02.js
@@ -1,5 +1,44 @@
-/* using callback 
-------------------*/
+/* 
+
+It seems like you are using this code in a Deno environment rather than Node.js. 
+
+The "require" function is specific to "CommonJS modules" used in Node.js and 
+is not available in Deno, which uses "ECMAScript modules (ESM)".
+
+In Deno, you can use the "import" syntax for module loading. 
+
+Here's how you can modify your code:
+
+import { writeTextFile } from "https://deno.land/std/fs/mod.ts";
+
+const data = "Hello, world!";
+
+writeTextFile("example.txt", data)
+  .then(() => {
+    console.log("File written successfully!");
+  })
+  .catch((error) => {
+    console.error("Error writing file:", error);
+  });
+
+
+In Deno, the standard library module for file operations is https://deno.land/std/fs/mod.ts. 
+
+The "writeTextFile" function is used for writing data to a file asynchronously. 
+
+Note that the syntax and APIs are different in Deno compared to Node.js.
+
+Ensure you have the necessary permissions to write to the file by running your 
+script with the "--allow-write" flag:
+
+--> bash
+
+deno run --allow-write app_02.js
+
+*/
+
+/* using callback in Node.js environment
+-----------------------------------------*/
 
 // const { writeFile } = require("fs");
 
@@ -9,10 +48,8 @@
 //   console.log("Written to file");
 // });
 
-//-----------------------------------------------------------
-
-/* using promises 
-------------------*/
+/* using promises in Node.js environment 
+----------------------------------------*/
 
 // const { writeFile } = require("fs").promises;
 
@@ -21,14 +58,45 @@
 //   console.log("Written to file");
 // });
 
-//-----------------------------------------------------------
+/* using async/await in Node.js environment 
+--------------------------------------------*/
 
-/* using async/await 
-------------------*/
+// const { writeFile } = require("fs").promises;
 
-const { writeFile } = require("fs").promises;
+// async function writeToTestFile() {
+//   const text = "This should be stored in a file again";
+
+//   try {
+//     await writeFile("test-nodejs.txt", text);
+//     console.log("Written to file");
+//   } catch (error) {
+//     console.error("Error writing to file:", error);
+//   }
+// }
+
+// writeToTestFile();
+
+/* in Deno environment: using await
+--------------------------------------------------*/
+
 // import { writeFile } from "node:fs/promises";
+// const text = "This should be store in a file again";
+// console.log("before: Written to file");
+// await writeFile("test-nodejs.txt", text).then(() => {
+//   console.log("after: Written to file");
+// });
+
+// console.log("Awaited for previous code to run !");
+
+/* in Deno environment: using .then()
+--------------------------------------------------*/
+
+import { writeFile } from "node:fs/promises";
 
 const text = "This should be store in a file again";
-writeFile("test-nodejs.txt", text);
-console.log("Written to file");
+console.log("before: Written to file");
+writeFile("test-nodejs.txt", text).then(() => {
+  console.log("after: Written to file");
+});
+
+console.log("Awaited for previous code to run ?");
--- 34_Introduction_to_Deno/01-first-deno-code/app_02.ts
+++ 34_Introduction_to_Deno/01-first-deno-code/app_02.ts
@@ -5,22 +5,42 @@ function Deno.writeFile(
     options?: Deno.WriteFileOptions | undefined
 ): Promise<void>
 
-Write data to the given path, by default creating 
-a new file if needed, else overwriting. */
+Write data to the given path, by default creating a new file if needed, else overwriting. 
 
-/*
 Requires allow-write permission, and allow-read if options.create is false.
-@tags - allow-read, allow-write  */
+@tags - allow-read, allow-write  
+
+---------------
+
+function Deno.writeFile(
+  path: string | URL, 
+  data: Uint8Array | ReadableStream<Uint8Array>, 
+  options?: Deno.WriteFileOptions | undefined
+): Promise<...>
+
+Write data to the given path, by default creating a new file if needed, else overwriting.
+
+const encoder = new TextEncoder();
+const data = encoder.encode("Hello world\n");
+await Deno.writeFile("hello1.txt", data);  // overwrite "hello1.txt" or create it
+await Deno.writeFile("hello2.txt", data, { create: false });  // only works if "hello2.txt" exists
+await Deno.writeFile("hello3.txt", data, { mode: 0o777 });  // set permissions on new file
+await Deno.writeFile("hello4.txt", data, { append: true });  // add data to the end of the file
+Requires allow-write permission, and allow-read if options.create is false.
+
+@tags - allow-read, allow-write
+
+*/
 
 const text = "This should be store in a file again \n";
 const encoder = new TextEncoder();
 const data = encoder.encode(text);
 console.log("data = ", data);
 
-Deno.writeFile("hello1.txt", data); // overwrite "hello1.txt" or create it
-Deno.writeFile("hello2.txt", data, { create: false, append: true }); // only works if "hello2.txt" exists
-Deno.writeFile("hello3.txt", data, { mode: 0o777 }); // set permissions on new file
-Deno.writeFile("hello4.txt", data, { append: true }); // add data to the end of the file
+await Deno.writeFile("hello1.txt", data); // overwrite "hello1.txt" or create it
+await Deno.writeFile("hello2.txt", data, { create: false, append: true }); // only works if "hello2.txt" exists
+await Deno.writeFile("hello3.txt", data, { mode: 0o777 }); // set permissions on new file
+await Deno.writeFile("hello4.txt", data, { append: true }); // add data to the end of the file
 
 Deno.writeFile("message-max.txt", data).then(() =>
   console.log("Written to file"),
